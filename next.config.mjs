23124b87dfbe1efac6ca8a14655e822abdc09794
ruanosena <ruosena@hotmail.com>
2024-02-21 19:19:07 -0300

    fix issue of import phosphor icons

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  modularizeImports: {
+    '@phosphor-icons/react': {
+      transform: '@phosphor-icons/react/dist/ssr/{{member}}',
+    },
+  },
+  experimental: {
+    optimizePackageImports: ['@phosphor-icons/react'],
+  },
 }
 
 export default nextConfig
